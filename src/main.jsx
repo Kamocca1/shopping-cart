2270762888b81500b5e34d56b18930dde725a5f5
Kamocca <86765533+Kamocca1@users.noreply.github.com>
2025-09-07 09:54:44 +0200

    chore: update dependencies and lockfile
    - Updated dependencies in package.json
    - Regenerated package-lock.json

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "./index.css";
+
+import App from "./App.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        // errorElement: <ErrorPage />,
+        // children: [
+        //     { index: true, element: <Hero /> },
+        //     { path: "shop", element: <Shop /> },
+        //     { path: "shop/favorites", element: <Favorites /> },
+        //     { path: "cart", element: <Cart /> },
+        // ],
+    },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <RouterProvider router={router} />
+    </StrictMode>
+);
